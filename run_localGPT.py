38c18459fbc742cb50a1737652d2a1d87e6760f9
Konstantin Goldman <konstantin.goldman@gmail.com>
2023-06-01 12:38:25 -0700

    Update code with remaining auto-generated blocks, and fix gitignore

--- run_localGPT.py
+++ run_localGPT.py
@@ -11,32 +11,15 @@ from constants import CHROMA_SETTINGS
 
 def load_model():
     '''
-    Select a model on huggingface. 
-    If you are running this for the first time, it will download a model for you. 
-    subsequent runs will use the model from the disk. 
+    Select a model on huggingface.
+    If you are running this for the first time, it will download a model for you.
+    subsequent runs will use the model from the disk.
     '''
     model_id = "TheBloke/vicuna-7B-1.1-HF"
     tokenizer = LlamaTokenizer.from_pretrained(model_id)
-
-    model = LlamaForCausalLM.from_pretrained(model_id,
-                                            #   load_in_8bit=True, # set these options if your GPU supports them!
-                                            #   device_map=1#'auto',
-                                            #   torch_dtype=torch.float16,
-                                            #   low_cpu_mem_usage=True
-                                              )
-
-    pipe = pipeline(
-        "text-generation",
-        model=model, 
-        tokenizer=tokenizer, 
-        max_length=2048,
-        temperature=0,
-        top_p=0.95,
-        repetition_penalty=1.15
-    )
-
+    model = LlamaForCausalLM.from_pretrained(model_id)
+    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=2048, temperature=0, top_p=0.95, repetition_penalty=1.15)
     local_llm = HuggingFacePipeline(pipeline=pipe)
-
     return local_llm
 
 @click.command()
